6bdbb89cf3bd7ffe93be934d4c37bd3a237d7bf3
alexH-Work <alex.hamwi55@gmail.com>
2021-06-04 14:42:27 -0400

    Hid passord API Key and implemented first api

--- server/index.js
+++ server/index.js
@@ -2,21 +2,28 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import API_KEY from './secrets.js';
+import questionRoutes from './routes/questions.js';
 
 const app = express();
+const CONNECTION_URL = API_KEY;
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+app.use(express.json());
 
 
-const CONNECTION_URL = 'mongodb+srv://dev-admin01:dev33password@cluster0.50mlu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+app.get('/', (req,res) =>{res.send("Hello From The Homepage")});
+
+ app.use(express.urlencoded({limit: "30mb", extended: true}));
+ app.use(cors());
+
 
 mongoose.connect(CONNECTION_URL, {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
-.then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
-.catch((error) =>  console.log(error.message));
+     useNewUrlParser: true, useUnifiedTopology: true
+ })
+ .then(() => {  
+    app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
+    })
+ .catch((error) =>  console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+ mongoose.set('useFindAndModify', false);
