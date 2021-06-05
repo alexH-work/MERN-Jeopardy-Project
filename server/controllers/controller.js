9d38b98960b7a152a54041f57f956958b36d5bcf
alexH-Work <alex.hamwi55@gmail.com>
2021-06-05 17:25:16 -0400

    got get api to get all stored data

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,16 +1,32 @@
 import PostQuestion from '../models/model.js';
 
-export const getQuestion = (req,res)=> {res.send(questions)}
+export const getQuestion = (req,res)=> {
+    try {
+        const questions = PostQuestion.find(
+            {}, function(err, result) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  res.json(result);
+                }
+              }
+        );
+
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
 
 export const createQuestion = (req,res)=> {
-    const question =req.body;
-    
+    const data =req;    
+    const newPostQuestion = new PostQuestion(data);
 
-    try {       
-        res.status(201).json(question);
+    try {   
+        newPostQuestion.save();    
+        res.status(201).json(newPostQuestion);
     } catch (error) {
         res.status(409).json({message: error.message});
     }
 
-    res.send(`${question.firstName} added to database`)
+    res.send(`${newPostQuestion} added to database`)
 }
